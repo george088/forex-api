e034a25c7598320a11b0f99ece3c9ffe69691fbc
george <gga1007001@ggmail.com>
2019-01-18 17:16:34 +0300

    1

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_141252) do
-
-  create_table "quotes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2019_01_18_082343) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
