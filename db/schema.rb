2b54aa97399136aca185e8a05c6bfc2eb8886f99
george <gga1007001@ggmail.com>
2019-01-18 17:23:50 +0300

    model ticketlist with access

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_141906) do
+ActiveRecord::Schema.define(version: 2019_01_18_142141) do
 
   create_table "quotes", force: :cascade do |t|
     t.float "open"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2019_01_18_141906) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "symbol"
+  end
+
+  create_table "ticketlists", force: :cascade do |t|
+    t.string "sybmol"
+    t.boolean "for_premium"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
