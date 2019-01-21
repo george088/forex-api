cdc8edb9ea7371ee593de1cef573e2159b1ad580
george <gga1007001@ggmail.com>
2019-01-21 14:09:06 +0300

    rails db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_142141) do
+ActiveRecord::Schema.define(version: 2019_01_21_104746) do
 
   create_table "quotes", force: :cascade do |t|
     t.float "open"
