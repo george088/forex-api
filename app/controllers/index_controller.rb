c1916a9e0d6fa73af01dad536c60e83ed5501f93
george <gga1007001@ggmail.com>
2019-01-22 19:04:57 +0300

    test show model

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,8 +1,14 @@
 class IndexController < ApplicationController
   before_action :authenticate_user!
   # before_action :set_todo, only: [:show, :edit, :update, :destroy]
+  before_action :load_allrows, only: [:index]
 
   def index
-
+    
   end
+
+  private
+    def load_allrows
+      @Quotes = Quote.all
+    end
 end
