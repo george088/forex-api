bb8d8826c8e9b0e75da38b78711dfbded7629f04
George <g@localhost.localdomain>
2019-01-28 16:12:51 +0300

    make quotes and ticket list

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.3'
 
+gem 'nokogiri', '1.8.5'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1', '>= 5.2.1.1'
 # Use sqlite3 as the database for Active Record
@@ -34,7 +36,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 # Reduces boot times through caching; required in config/boot.rb
-gem 'bootsnap', '>= 1.1.0', require: false
+gem 'bootsnap', '>= 1.3.2', require: false
 
 # Slim HTML generator
 gem 'slim-rails'
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,23 +1,45 @@
 class ApiController < ApplicationController
+  before_action :get_user
 
   # /api/v1/tickets_list?key=...
   def tickets_list
-    key = params[:key]
+    return render json: 'key missing', status: :not_acceptable unless params.has_key?(:key)
+    return render json: 'user not_found', status: :not_found unless User.exists?(apikey: params[:key])
 
-    api_current_user = User.find_by(api_key: key)
     ticketlist = 0
 
-    if api_current_user.role == 'premium'
-      ticketlist = Ticketlist.all.pluck(:role)
+    if @api_current_user.role == 'premium'
+      ticketlist = Ticketlist.all.pluck(:sybmol)
     else
-      ticketlist = Ticketlist.find_by(for_premium: '0').pluck(:role)
+      ticketlist = Ticketlist.find_by(for_premium: false)
     end
 
-    render json: {'tickets' : ticketlist}, status: :ok
+    render json:  ticketlist, status: :ok
   end
 
   # api/v1/quotes?key=?&ticket=?&type=(OHLC/close)&from=YYYY-mm-dd&to=YYYY-mm-dd
   def quotes
-    params.values.all?(:present?)
+    check_params = [:key, :ticket, :type, :from, :to]
+
+    return render json: {'Missing keys': "#{check_params - params.keys}"}, status: :not_acceptable if (check_params - params.keys).size > 0
+    return render json: {'Missing values': "not values"}, status: :not_acceptable unless params.values.all?(:present?)
+    return render json: {'Missing dates': 'wrong format'}, status: :not_acceptable if (date_validate(params[:from]).nil? || date_validate(params[:to].nil?))
+    date_from = Date.parse(params[:from])
+    date_to = Date.parse(params[:to])
+
+    return render json: {'Missing dates': 'wrong format'}, status: :not_acceptable if date_from > date_to
+
+    Quotes.where(days: {:date => date_from..date_to}, symbol: params[:ticket]).pluck(:open, :high, :low, :close, :symbol)
+
   end
+
+  private
+
+    def get_user
+      @api_current_user = User.find_by(apikey: params[:key])
+    end
+
+    def date_validate date
+      Date.parse date rescue nil
+    end
 end
