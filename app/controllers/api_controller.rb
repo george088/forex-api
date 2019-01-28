3e1c9cbaa8c8270549e81f5d9d26a75273317531
George <g@localhost.localdomain>
2019-01-28 16:30:13 +0300

    check user auth by key

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,10 @@
 class ApiController < ApplicationController
+  before_action :user_auth_by_key
   before_action :get_user
-
   # /api/v1/tickets_list?key=...
   def tickets_list
     return render json: 'key missing', status: :not_acceptable unless params.has_key?(:key)
-    return render json: 'user not_found', status: :not_found unless User.exists?(apikey: params[:key])
+
 
     ticketlist = 0
 
@@ -19,17 +19,17 @@ class ApiController < ApplicationController
 
   # api/v1/quotes?key=?&ticket=?&type=(OHLC/close)&from=YYYY-mm-dd&to=YYYY-mm-dd
   def quotes
-    check_params = [:key, :ticket, :type, :from, :to]
-
-    return render json: {'Missing keys': "#{check_params - params.keys}"}, status: :not_acceptable if (check_params - params.keys).size > 0
-    return render json: {'Missing values': "not values"}, status: :not_acceptable unless params.values.all?(:present?)
-    return render json: {'Missing dates': 'wrong format'}, status: :not_acceptable if (date_validate(params[:from]).nil? || date_validate(params[:to].nil?))
+    check_params = ['key', 'ticket', 'type', 'from', 'to']
+    # return render json: params.keys
+    return render json: {'Missing keys': "#{check_params - params.keys}"}, status: :ok if (check_params - params.keys).size > 0
+    return render json: {'Missing values': "not values"}, status: :ok if params.values.all?(:present?)
+    return render json: {'Missing dates': 'wrong format'}, status: :ok if (date_validate(params[:from]).nil? || date_validate(params[:to].nil?))
     date_from = Date.parse(params[:from])
     date_to = Date.parse(params[:to])
 
-    return render json: {'Missing dates': 'wrong format'}, status: :not_acceptable if date_from > date_to
+    return render json: {'Missing dates': 'wrong format'}, status: :ok if date_from > date_to
 
-    Quotes.where(days: {:date => date_from..date_to}, symbol: params[:ticket]).pluck(:open, :high, :low, :close, :symbol)
+    render json: Quote.where(symbol: params[:ticket], datestamp: date_from..date_to).pluck(:open, :high, :low, :close, :symbol, :datestamp)
 
   end
 
@@ -41,5 +41,9 @@ class ApiController < ApplicationController
 
     def date_validate date
       Date.parse date rescue nil
+    end
+
+    def user_auth_by_key
+      return render json: 'user not_found', status: :not_found unless User.exists?(apikey: params[:key])
     end
 end
