90cea4289054ccf3b961f4640c0412ac914093d7
George <g@localhost.localdomain>
2019-01-28 16:52:24 +0300

    add quotes_for_date

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -26,11 +26,23 @@ class ApiController < ApplicationController
     return render json: {'Missing dates': 'wrong format'}, status: :ok if (date_validate(params[:from]).nil? || date_validate(params[:to].nil?))
     date_from = Date.parse(params[:from])
     date_to = Date.parse(params[:to])
-
     return render json: {'Missing dates': 'wrong format'}, status: :ok if date_from > date_to
 
-    render json: Quote.where(symbol: params[:ticket], datestamp: date_from..date_to).pluck(:open, :high, :low, :close, :symbol, :datestamp)
+    render json: Quote.where(symbol: params[:ticket], datestamp: for_date).pluck(:open, :high, :low, :close, :symbol, :datestamp) if type == 'OHLC'
+    render json: Quote.where(symbol: params[:ticket], datestamp: for_date).pluck(:close, :symbol, :datestamp) if type == 'close'
+  end
 
+  # /v1/quotes_for_date?key=user_api-key&type=(OHLC/close)&for_date=YYYY-mm-dd
+  def quotes_for_date
+    check_params = ['key', 'for_date', 'type']
+    # return render json: params.keys
+    return render json: {'Missing keys': "#{check_params - params.keys}"}, status: :ok if (check_params - params.keys).size > 0
+    return render json: {'Missing values': "not values"}, status: :ok if params.values.all?(:present?)
+    return render json: {'Missing dates': 'wrong format'}, status: :ok if (date_validate(params[:for_date]).nil?))
+    date_from = Date.parse(params[:for_date])
+
+    render json: Quote.where(datestamp: for_date).pluck(:open, :high, :low, :close, :symbol, :datestamp) if type == 'OHLC'
+    render json: Quote.where(datestamp: for_date).pluck(:close, :symbol, :datestamp) if type == 'close'
   end
 
   private
@@ -45,5 +57,9 @@ class ApiController < ApplicationController
 
     def user_auth_by_key
       return render json: 'user not_found', status: :not_found unless User.exists?(apikey: params[:key])
+    end
+
+    def choose_type type
+       if type == 'OHLC'
     end
 end
